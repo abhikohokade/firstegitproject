0d3525375211130eb540d3a9d5bfa6cb1c74d710
abhikohokade <genericabhi@gmail.com>
2014-12-27 12:28:10 +0530

    First testing of egit from eclipse

---
+++ gittesting/src/com/lots/testing/git/Shiv.java
@@ -0,0 +1,11 @@
+package com.lots.testing.git;
+
+public class Shiv {
+	public Shiv() {
+		System.out.println("shiv chidambar");
+	}
+
+	public static void main(String[] args) {
+		new Shiv();
+	}
+}
